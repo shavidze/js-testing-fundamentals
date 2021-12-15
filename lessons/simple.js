3cab05a11fc266e1bc16275c02670cda41367db9
Saba <sshav@tula.co>
2021-12-15 15:06:52 +0400

    simple test example with message

--- lessons/simple.js
+++ lessons/simple.js
@@ -1,1 +1,32 @@
-const {sum, substract} = require("../math")
+const {sum, substract} = require("../math");
+
+let result, expected;
+
+
+ function sumTest() {
+    result = substract(4,6);
+    expected = -2;
+    expect(result).toBe(expected);
+ }
+test('sum adds numbers', sumTest);
+ 
+ function test(title, callback) {
+    try {
+        callback();
+        console.log(`✓ ${title}`)
+    } catch (error) {
+        console.error(`x ${title}`);
+        console.error(error);
+    }
+ }
+
+
+function expect(actual) {
+    return {
+        toBe(expected) {
+            if(actual !== expected) {
+                throw new Error(`${actual} is not equal to ${expected}`)
+            }
+        }
+    }
+}
